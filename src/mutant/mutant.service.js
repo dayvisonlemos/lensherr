30024bf34d2f2deed5dc5ff23c9df65ff630e449
Dayvison Lemos <dayvison.lemos@gmail.com>
2018-09-02 21:03:51 -0300

    Adicionando verificação lint

--- src/mutant/mutant.service.js
+++ src/mutant/mutant.service.js
@@ -1,7 +1,6 @@
 const matrix = require('../utilities/matrix');
 
 module.exports = class MutantService {
-
   constructor() {
     this.allowedChar = /^[A|T|C|G|. ]+$/;
     this.pattern = /((A+){4}|(T+){4}|(C+){4}|(G+){4})/g;
@@ -15,21 +14,18 @@ module.exports = class MutantService {
   isMutant(dna) {
     const dnaMatrix = matrix.fromDnaSequence(dna);
 
-    if (!matrix.isQuadratic(dnaMatrix))
-      throw new Error('There is an error in this DNA sequence. Not a NxN utilities.');
+    if (!matrix.isQuadratic(dnaMatrix)) throw new Error('There is an error in this DNA sequence. Not a NxN utilities.');
 
     let sequence = matrix.toString(dnaMatrix);
 
-    if (!this.allowedChar.test(sequence))
-      throw new Error('There is an error in this DNA sequence. Not alowed chars.');
+    if (!this.allowedChar.test(sequence)) throw new Error('There is an error in this DNA sequence. Not alowed chars.');
 
     const transposed = matrix.transpose(dnaMatrix);
     const transversed = matrix.transverse(dnaMatrix);
 
     sequence = `${sequence} ${matrix.toString(transposed)} ${matrix.toString(transversed)}`;
-    let countDnaMutant = this.analizeSequence(sequence);
+    const countDnaMutant = this.analizeSequence(sequence);
 
     return countDnaMutant > 1;
   }
-
 };
--- src/utilities/matrix.js
+++ src/utilities/matrix.js
@@ -7,16 +7,21 @@ function transpose(matrix) {
 }
 
 function getTransverse(col, matrix, reverse) {
-  const rowLRUp = [], rowLRDown = [], rowRLUp = [], rowRLDown = [];
-  let j = 0, n = matrix.length - 1;
+  const rowLRUp = [];
+  const rowLRDown = [];
+  const rowRLUp = [];
+  const rowRLDown = [];
+  let j = 0;
+  let n = matrix.length - 1;
   const rows = [];
   if (reverse) {
-    for (let i = col; i >= 0; i--) {
-      rowRLUp.push(matrix[j++][i]);
+    for (let i = col; i >= 0; i -= 1) {
+      rowRLUp.push(matrix[j][i]);
+      j += 1;
     }
     rows.push(rowRLUp);
   } else {
-    for (let i = col; i < matrix.length; i++) {
+    for (let i = col; i < matrix.length; i += 1) {
       rowLRUp.push(matrix[j][i]);
       if (i !== j) {
         rowLRDown.push(matrix[i][j]);
@@ -26,10 +31,8 @@ function getTransverse(col, matrix, reverse) {
       n -= 1;
     }
     rows.push(rowLRUp);
-    if (rowLRDown.length > 0)
-      rows.push(rowLRDown);
-    if (rowRLDown.length > 0)
-      rows.push(rowRLDown);
+    if (rowLRDown.length > 0) rows.push(rowLRDown);
+    if (rowRLDown.length > 0) rows.push(rowRLDown);
   }
   return rows;
 }
@@ -37,10 +40,11 @@ function getTransverse(col, matrix, reverse) {
 function transverse(matrix) {
   let transversed = [];
   let j = matrix.length - 1;
-  for (let i = 0; i < matrix.length; i++) {
+  for (let i = 0; i < matrix.length; i += 1) {
     transversed = transversed
       .concat(getTransverse(i, matrix, false))
-      .concat(getTransverse(j--, matrix, true));
+      .concat(getTransverse(j, matrix, true));
+    j -= 1;
   }
   return transversed;
 }
@@ -52,8 +56,8 @@ function toString(matrix) {
 function isQuadratic(matrix) {
   let result = true;
   const N = matrix.length;
-  for (let n in matrix) {
-    if (N !== matrix[n].length) {
+  for (let i = 0; i < matrix.length; i += 1) {
+    if (N !== matrix[i].length) {
       result = false;
       break;
     }
@@ -66,5 +70,5 @@ module.exports = {
   transpose,
   transverse,
   isQuadratic,
-  toString
+  toString,
 };
